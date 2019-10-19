41e44305a56604193b96d4c21bbee0650762767b
Sophia Cheung <cheungsinhan@gmail.com>
2019-10-19 16:38:52 -0700

    Add login

--- techtonica/src/layout/authModal.js
+++ techtonica/src/layout/authModal.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import './authModal.css';
 
-const AuthModal = () => {
+const AuthModal = (props) => {
+  const [ modalData, setModalData ] = useState({
+    hidden: props.toggle,
+    email: ""
+  });
+
+  const { hidden, email } = modalData;
+
+  const handleChange = (e) => {
+    setModalData({ ...modalData, email: e.target.value });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    localStorage.email = email;
+    setModalData({ ...modalData, hidden: true });
+  }
 
   return(
-    <>
-      {/* <div className="modal-container-on">
-        <a href="/studentSubmission/:topic">Submit</a>
-      </div> */}
-    </>
+    hidden ? <></> : (
+      <div className="modal-container">
+
+        <h3>Please sign in with your email.</h3>
+        <form className="signin" onSubmit={handleSubmit}>
+
+          <label>
+            Email:
+            <input type="email" name="email" value={email} onChange={handleChange}/>
+          </label>
+
+          <input type="submit" value="Log In" />
+
+        </form>
+
+      </div>
+    )
   )
 }
 
--- techtonica/src/layout/nav.js
+++ techtonica/src/layout/nav.js
@@ -1,23 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import './nav.css';
 import AuthModal from './authModal';
 
 const Nav = () => {
+  const email = localStorage.email;
+  const [ loggedIn, setLoggedIn, toggle, setToggle ] = useState(!!email.length);
+  const [ modalData, setModalData ] = useState({
+    hidden: false,
+    email: ""
+  });
+
+  const logout = () => {
+    localStorage.email = "";
+    setLoggedIn({ loggedIn: false })
+  }
+
+  const showLogout = () => (
+    <>
+      <span>You're currently logged in under {email}.</span>
+      <span className="session-btn" onClick={logout}>Log Out</span>
+    </>
+  )
+
+  const showLogin = () => (
+    <span className="session-btn" onClick={() => setToggle(!toggle)}>Login</span>
+  )
+
+  const showModal = () => (
+    <div className="modal-container">
+
+      <h3>Please sign in with your email.</h3>
+      <form className="signin" onSubmit={handleSubmit}>
+
+        <label>
+          Email:
+            <input type="email" name="email" value={email} onChange={handleChange} />
+        </label>
+
+        <input type="submit" value="Log In" />
+
+      </form>
+
+    </div>
+  )
+
+  const handleChange = (e) => {
+    setModalData({ ...modalData, email: e.target.value });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    localStorage.email = email;
+    setModalData({ ...modalData, hidden: true });
+  }
 
   return(
-    <>
-      <nav>
-        <div className="nav-left">
-          <a href="/">
-            <img src={process.env.PUBLIC_URL + '/img/techtonica_logo.png'} alt="techtonica logo" className="nav-logo"/>
-          </a>
-        </div>
-        <div className="nav-right">
-          <a href="/studentSubmission/:topic">Login</a>
-        </div>
-      </nav>
-    </>
+    <nav>
+
+      <div className="nav-left">
+        <a href="/">
+          <img src={process.env.PUBLIC_URL + '/img/techtonica_logo.png'} alt="techtonica logo" className="nav-logo"/>
+        </a>
+      </div>
+      
+      <div className="nav-right">
+        { loggedIn ? showLogout() : showLogin() }
+      </div>
+
+      { toggle ? <></> : showModal() }
+
+    </nav>
   )
 }
 
