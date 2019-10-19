0cb57392e7427642b62829d4b12f7ebb463a6687
Sophia Cheung <cheungsinhan@gmail.com>
2019-10-19 14:07:46 -0700

    Finish basic form

--- techtonica/src/replicated/studentSubmission.js
+++ techtonica/src/replicated/studentSubmission.js
@@ -1,58 +1,119 @@
-import React from 'react';
-// import { withRouter } from 'react-router-dom';
+import React, { useState } from 'react';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
 import './studentSubmission.css';
 
-const StudentSubmission = () => {
-
-  function handleSubmit(){
-
-  }
-
-  function getTopic(){
-
-  }
-
-  function getEmail(){
-
-  }
+const StudentSubmission = (props) => {
+  const [ userData, setUserData ] = useState({
+    email: localStorage.email,
+    topic: props.match.params.topic,
+    url: '',
+    helpful: null,
+    time: null,
+    difficulty: null
+  });
   
+  const { email, topic, url } = userData;
+
+  const handleChange = (e) => {
+    setUserData({...userData, [e.target.name]: e.target.value });
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+    console.dir(userData);
+  }
+
   return(
     <div className="submission-body">
-      <form className="student_form" onSubmit={handleSubmit()}>
-        <input type="hidden" name="email" value={getEmail()}></input>
+      <form className="student_form" onSubmit={handleSubmit}>
+        <input type="hidden" name="email" value={email} ></input>
 
-        <label>Topic: 
-          <input id="topic" type="text" name="topic" value={getTopic()} />
-        </label>
+        <h1>Topic: {topic} </h1>
 
         <label>Github Link: 
-          <input id="url" type="url" name="url" value="" ></input>
+          <input id="url" type="url" name="url" value={url} 
+            onChange={handleChange} />
         </label>
 
-        <label>How long did it take? 
-          <input type="range" list="tickmarks">
+        <div className="helpful">
+          <h3>How helpful was this topic/assignment?</h3>
 
-            <datalist id="tickmarks">
-              <option value="<1 hr"></option>
-              <option value="~1 hr"></option>
-              <option value="~2 hrs"></option>
-              <option value="~3 hrs"></option>
-              <option value="~4 hrs"></option>
-              <option value="~5 hrs"></option>
-              <option value=">5 hrs"></option>
-            </datalist>
+          <label>Not at All
+            <input type="radio" name="helpful" value="1"
+              onChange={handleChange} />
+          </label>
 
-          </input>
+          <label>Somewhat
+            <input type="radio" name="helpful" value="2"
+              onChange={handleChange} />
+          </label>
 
-        </label>
+          <label>Very
+            <input type="radio" name="helpful" value="3"
+              onChange={handleChange} />
+          </label>
 
-        <label>Difficulty (1 = easiest, 4 = hardest): 
-          <input id="time" type="number" name="time" value="" ></input>
-        </label>
+          <label>Amazingly
+            <input type="radio" name="helpful" value="4"
+              onChange={handleChange} />
+          </label>
+          
+        </div>
 
+        <div className="time">
+          <h3>How long did it take?</h3>
+
+          <label>less than 1 hour
+            <input type="radio" name="time" value="1"
+              onChange={handleChange} />
+          </label>
+
+          <label>~1 hour
+            <input type="radio" name="time" value="2"
+              onChange={handleChange} />
+          </label>
+
+          <label>~2 hours
+            <input type="radio" name="time" value="3"
+              onChange={handleChange} />
+          </label>
+
+          <label>more than 2 hours
+            <input type="radio" name="time" value="4"
+              onChange={handleChange} />
+          </label>
+          
+        </div>
+
+        <div className="difficulty">
+          <h3>How difficult was it?</h3>
+
+          <label>Easy Breezy
+            <input type="radio" name="difficulty" value="1"
+              onChange={handleChange} />
+          </label>
+
+          <label>Alright
+            <input type="radio" name="difficulty" value="2"
+              onChange={handleChange} />
+          </label>
+
+          <label>A Bit Challenging
+            <input type="radio" name="difficulty" value="3"
+              onChange={handleChange} />
+          </label>
+
+          <label>Challenging
+            <input type="radio" name="difficulty" value="4"
+              onChange={handleChange} />
+          </label>
+          
+        </div>
+        
+        <input type="submit" value="submit" />
       </form>
     </div>
   )
 }
 
-export default StudentSubmission;
+export default withRouter(StudentSubmission);
